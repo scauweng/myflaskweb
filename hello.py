d612fd89bf9228a95833dba604bb2d5ca53b14f3
scauweng <nihaoamo@qq.com>
2016-09-12 16:00:08 +0800

    160912

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
+from threading import Thread
 from flask import Flask,render_template,redirect,session,url_for,flash
-from flask_script import Manager
+from flask_script import Manager,Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate,MigrateCommand
+from flask_mail import Mail,Message
+
+import os
 
 app=Flask(__name__)
 
@@ -14,6 +20,60 @@ app.config['SECRET_KEY']='hard to guess string'
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+
+
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS']=True	
+app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = '545673470@qq.com'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
+# mail=Mail(app)   must put after the "app.config".
+mail=Mail(app)
+
+
+def send_async_email(app,msg):
+	with app.app_context():
+		mail.send(msg)
+
+def send_email(to,subject,template,**kwargs):
+	msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,\
+		sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+	msg.body=render_template(template+'.txt',**kwargs)
+	msg.html=render_template(template+'.html',**kwargs)
+
+	thr=Thread(target=send_async_email,args=[app,msg])
+	thr.start()
+	return thr
+
+'''
+def send_email(to,subject,template,**kwargs):
+
+	msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,
+		sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+	msg.body=render_template(template+'.txt',**kwargs)
+	msg.html=render_template(template+'.html',**kwargs)
+	mail.send(msg)
+'''
+
+def make_shell_context(): 
+	'''auto import something'''
+	return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
 
 
 class NameForm(Form):
@@ -25,12 +85,22 @@ class NameForm(Form):
 def index():
 	form=NameForm()
 	if form.validate_on_submit():
-		old_name=session.get('name')
-		if old_name is not None and old_name!=form.name.data:
-			flash('Looks like you have changed your name!')
+		user=User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user=User(username=form.name.data)
+			db.session.add(user)
+			session['known']=False
+			if app.config['FLASKY_ADMIN']:
+				send_email(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
+
+		else:
+			session['known']=True
 		session['name']=form.name.data
+		form.name.data=''
+
 		return redirect(url_for('index'))  
-	return render_template('index.html',form=form,name=session.get('name'))
+	return render_template('index.html',form=form,name=session.get('name'),
+		known=session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -44,6 +114,24 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'),500
+
+class Role(db.Model):
+	__tablename__='roles'
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(64),unique=True)
+	users=db.relationship('User',backref='role',lazy='dynamic')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer,primary_key=True)
+	username=db.Column(db.String(64),unique=True,index=True)
+	role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 if __name__=='__main__':
 	manager.run()
